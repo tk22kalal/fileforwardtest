f3a5300bc09d0ab407a91646270855446dbf8a7a
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2026-05-24 22:59:34 +0530

    Update progress.py

--- main/plugins/progress.py
+++ main/plugins/progress.py
@@ -5,7 +5,7 @@ import json
 from main.plugins.helpers import TimeFormatter, humanbytes
 
 #------
-FINISHED_PROGRESS_STR = "🟢"
+FINISHED_PROGRESS_STR = "🤢"
 UN_FINISHED_PROGRESS_STR = "🔴"
 DOWNLOAD_LOCATION = "/app"
 
